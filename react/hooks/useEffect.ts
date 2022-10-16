b1ed1be13bab127f75ab17ab1dedaad055f18055
Chewie94 <mykhailo.butkevych@gmail.com>
2022-10-16 22:01:24 +0400

    init

---
+++ react/hooks/useEffect.ts
@@ -0,0 +1,4 @@
+//REMINDERS:
+    // 1. Use Effects only for code that should run because the component was displayed to the user
+    // 2. An Effect describes how to synchronize an external system to the current props and state
+    // 3. Each Effect in your code should represent a separate and independent synchronization process.
